2bb5c09bcdb083279ca6d0f41ad295ac699c7806
Sushma92 <nuthanap1992@gmail.com>
2019-11-26 22:41:43 -0600

    publisher subscriber problem

---
+++ sushma.java.publisher-subsciber/src/main/java/com/sushma/java/publisher_subsciber/App.java
@@ -0,0 +1,13 @@
+package com.sushma.java.publisher_subsciber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
